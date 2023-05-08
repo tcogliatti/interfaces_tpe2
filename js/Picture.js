0443407f08cf7ae469331d1f09c94e20c90c8149
TC <tc@MacBook-Pro-de-TC.local>
2023-05-07 23:22:58 -0300

    migracion de blur terminada y funcionando

--- js/Picture.js
+++ js/Picture.js
@@ -93,119 +93,32 @@ class Picture {
         let calculator = new CalcSatu(0.1);
         this.aplicarFiltro(calculator);
     }
+    filterBlur(){
+        // instancia objeto de calculo de filtro
+        let calculator = new CalcBlur(4, this.imageData, this.width, this.heigth);
+        this.aplicarFiltro(calculator);
+    }
     aplicarFiltro(calculator){
         this.clear();
 
         let data = this.imageData.data;
-        for (let i = 0; i < data.length; i += 4) {
-            let r = data[i];
-            let g = data[i + 1];
-            let b = data[i + 2];
-            // obtener valor del 
-            let res = calculator.calcFilter(r,g,b);
-
-            // aplicar al pixel el nuevo valor
-            data[i]     = res['r'];
-            data[i + 1] = res['g'];
-            data[i + 2] = res['b'];
-            }
-
-        // Mostrar la imagen filtrada en el canvas
-        this.ctx.putImageData(this.imageData, this.posX, this.posY);
-    }
-
-    ///////////////////////////////// BLUR
-    filterBlur(){
-        this.clear(); 
-
-        let ratio = 2;
-        let data = this.imageData.data;
-
-        for (let x = 0; x < this.width; x++) {
+        for (let x = 0; x < this.width; x++)
             for (let y = 0; y < this.heigth; y++) {
-               let prom = this.getMatrixProm(ratio, x, y);
                let i = (x + y * this.width) * 4;
-               data[i]     = prom['r'];
-               data[i + 1] = prom['g'];
-               data[i + 2] = prom['b'];
+
+                let r = data[i];
+                let g = data[i + 1];
+                let b = data[i + 2];
+                // obtener valor del 
+                let res = calculator.calcFilter(r,g,b,x,y);
+
+                // aplicar al pixel el nuevo valor
+                data[i]     = res['r'];
+                data[i + 1] = res['g'];
+                data[i + 2] = res['b'];
             }
-        }
 
         // Mostrar la imagen filtrada en el canvas
         this.ctx.putImageData(this.imageData, this.posX, this.posY);
-    }
-    getMatrixProm(ratio, x, y){
-        let sum = {'r':0, 'g':0, 'b':0};
-        let count = 0;
-
-        // primeras filas
-        if(y < ratio){
-            // console.log('primeras filas', y, ratio);
-            for(let iy = 0; iy < (y+ratio); iy++){
-                let res = this.xIterations(x, iy, sum, count, ratio);
-                sum = res['sum'];
-                count = res['count'];
-            }
-        }
-        // ultimas filas
-        else if(this.heigth-1-y < ratio ){
-            // console.log('ultimas filas', y, this.heigth,'desde',y-ratio,'hasta',(y+(this.heigth-1-y)));
-            for(let iy = y-ratio; iy <= (this.heigth-1); iy++){
-                let res = this.xIterations(x, iy, sum, count, ratio);
-                sum = res['sum'];
-                count = res['count'];
-                // console.log(iy);
-            }
-        }
-        // filas del medio
-        else{
-            // console.log(' filas del medio');
-            // console.log('centro',y,'desde', y-(ratio+1), 'hasta', (y+(ratio+1)));
-            for(let iy = y-ratio; iy < (y+ratio); iy++){
-                let res = this.xIterations(x, iy, sum, count, ratio);
-                sum = res['sum'];
-                count = res['count'];
-                // console.log(iy);
-
-            }
-        }
-        return {
-            'r': sum['r'] / count,
-            'g': sum['g'] / count,
-            'b': sum['b'] / count
-        };
-    }
-    xIterations(x, iy, sum, count, ratio){
-            // primeras columnas
-            if(x < ratio)
-                for(let ix = 0; ix <= (x+ratio); ix++){
-                    sum = this.sumPixel(ix, iy, sum);
-                    count++;
-                }
-            // ultimas columnas
-            else if(this.width-1-x < ratio)
-                for(let ix = x-ratio; ix <= (this.width-1); ix++){
-                    sum = this.sumPixel(ix, iy, sum);
-                    count++;
-                }
-            // columnas del medio
-            else
-                for(let ix = x-ratio; ix < x+ratio; ix++){
-                    sum = this.sumPixel(ix, iy, sum);
-                    count++;
-                }
-
-        return {'sum': sum, 'count': count};
-    }
-    sumPixel(x, y, sum){
-        let data = this.imageData.data;  // obtenemos la informacion del backup asi no tenemos datos de input distorcionados
-        let index = (x + y * this.width) * 4;
-        let r = data[index + 0];
-        let g = data[index + 1];
-        let b = data[index + 2];
-        sum['r'] += r;
-        sum['g'] += g;
-        sum['b'] += b;
-        return sum;
     }
 }
--- js/filters/CalcBlur.js
+++ js/filters/CalcBlur.js
@@ -1,16 +1,77 @@
 class CalcBlur extends Operador{
-    constructor(intensity){
+    constructor(intensity, imgDat, width, heigth){
         super();
         this.intensity = intensity;
+        this.imgDat = imgDat;
+        this.heigth = heigth;
+        this.width = width;
     }
 
-    calcFilter(r,g,b){
-        let prom = (r + g + b)/3
-        let binary;
-        if(prom > this.intensity)
-            binary = 255;
-        else
-            binary = 0;
-        return {'r':binary, 'g':binary, 'b':binary};
+    calcFilter(r,g,b,x,y){
+        let sum = {'r':0, 'g':0, 'b':0};
+        let count = 0;
+
+        // primeras filas
+        if(y < this.intensity){
+            for(let iy = 0; iy < (y+this.intensity); iy++){
+                let res = this.xIterations(x, iy, sum, count, this.intensity);
+                sum = res['sum'];
+                count = res['count'];
+            }
+        }
+        // ultimas filas
+        else if(this.heigth-1-y < this.intensity ){
+            for(let iy = y-this.intensity; iy <= (this.heigth-1); iy++){
+                let res = this.xIterations(x, iy, sum, count, this.intensity);
+                sum = res['sum'];
+                count = res['count'];
+            }
+        }
+        // filas del medio
+        else{
+            for(let iy = y-this.intensity; iy < (y+this.intensity); iy++){
+                let res = this.xIterations(x, iy, sum, count, this.intensity);
+                sum = res['sum'];
+                count = res['count'];
+            }
+        }
+        return {
+            'r': sum['r'] / count,
+            'g': sum['g'] / count,
+            'b': sum['b'] / count
+        };
+    }
+    xIterations(x, iy, sum, count){
+            // primeras columnas
+            if(x < this.intensity)
+                for(let ix = 0; ix <= (x+this.intensity); ix++){
+                    sum = this.sumPixel(ix, iy, sum);
+                    count++;
+                }
+            // ultimas columnas
+            else if(this.width-1-x < this.intensity)
+                for(let ix = x-this.intensity; ix <= (this.width-1); ix++){
+                    sum = this.sumPixel(ix, iy, sum);
+                    count++;
+                }
+            // columnas del medio
+            else
+                for(let ix = x-this.intensity; ix < x+this.intensity; ix++){
+                    sum = this.sumPixel(ix, iy, sum);
+                    count++;
+                }
+
+        return {'sum': sum, 'count': count};
+    }
+    sumPixel(x, y, sum){
+        let data = this.imgDat.data;  
+        let index = (x + y * this.width) * 4;
+        let r = data[index + 0];
+        let g = data[index + 1];
+        let b = data[index + 2];
+        sum['r'] += r;
+        sum['g'] += g;
+        sum['b'] += b;
+        return sum;
     }
 }
